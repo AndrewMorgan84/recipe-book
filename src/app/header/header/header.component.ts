3cf34ecabea537155d63deaa23726da47c884f9d
Andy <Andyclem84@hotmail.com>
2020-09-07 20:36:58 +0100

    Added put method to save recipes to firebase db

--- src/app/header/header/header.component.ts
+++ src/app/header/header/header.component.ts
@@ -1,3 +1,4 @@
+import { DataStorageService } from './../../shared/data-storage.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
   collapsed = true;
 
-  constructor() { }
+  constructor(
+    private dataStorageService: DataStorageService
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSaveData(){
+    this.dataStorageService.storeRecipes()
+  }
+
+  onFetchData(){
+    this.dataStorageService.fetchRecipes();
   }
 
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,27 @@
+import { RecipeService } from './../recipes/recipe.service';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class DataStorageService {
+private dataBaseUrl = 'https://ng-course-recipe-book-63cd9.firebaseio.com/recipes.json';
 
 constructor(
-  private http: HttpClient
+  private http: HttpClient,
+  private recipeService: RecipeService
 ){}
 
+storeRecipes(){
+  const recipes = this.recipeService.getRecipes();
 
+  this.http.put(this.dataBaseUrl, recipes)
+  .subscribe(response => {
+    console.log(response);
+  });
+}
+
+fetchRecipes(){
+
+}
 
 }
